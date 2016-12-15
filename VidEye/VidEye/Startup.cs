a91a3d2603bbaf8449182b3aab0936c0214d5f31
Django2016 <frankbelinga@gmail.com>
2016-12-15 14:59:03 +0000

    first push video player

---
+++ VidEye/VidEye/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidEye.Startup))]
+namespace VidEye
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
